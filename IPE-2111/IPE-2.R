6aac4223422d26b2a8c60fc0f6e358731da17f90
astorlucas <lucasastor@gmail.com>
2021-11-20 15:12:29 -0300

    ejercicio 1

---
+++ IPE-2111/IPE-2.R
@@ -0,0 +1,43 @@
+v = (1:10^4)
+
+
+for(i in 1:10^4){
+  z=rnorm(1, mean=-4, sd=sqrt(16)/50)
+  v[i] <- z
+  
+}
+# Presentamos en un histograma los valores obtenidos en el loop
+s = seq(0, 5, 0.1)
+hist(v,
+     main="Promedio estandarizados y distribución normal estándar",
+     breaks = 20,
+     xaxt="n",
+     ylab="Densidad",
+     xlab="x",
+     freq=FALSE)
+# Sobre el histograma graficamos la densidad de la distribución normal estándar
+lines(s, 
+      dnorm(s, mean = -4, sd = sqrt(16)/50),
+      lty = 1, 
+      lwd = 2)
+
+polygon(s, 
+        dnorm(s, mean = -4, sd = sqrt(16)/50), 
+        col = rgb(1, 0, 0, alpha = 0.5))
+# Por último agregamos una leyenda para visualizar claramente los resultados  y cambiamos la escala del eje
+axis(1, at=s,las=1)
+legend("topleft", 
+       legend = c("Promedio estandarizado",
+                  "Distribución normal estándar"),
+       lty = 1, col = c("grey","red"), lwd =1, box.lty =1)
+#Falta arreglarr NORMAL ESTANDAR
+#Parte c)
+#10^4 intervalos de confianza con confianza 95%
+n <- 50    # El tamaño válido de la muestra
+media <- 32 # la media 
+desv <- 6  # La desviación estándar. Datos históricos
+nivelconfianza = 0.90
+
+zsum.test(mean.x=32,sigma.x=6, n.x=50,conf.level=0.90)
+
+
