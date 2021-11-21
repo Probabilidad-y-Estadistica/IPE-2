9ebe16d61bb97caa8d597db4f2978098ed427cb3
astorlucas <lucasastor@gmail.com>
2021-11-21 16:22:09 -0300

    normal hecha

--- IPE-2111/IPE-2.R
+++ IPE-2111/IPE-2.R
@@ -2,27 +2,37 @@ v = (1:10^4)
 
 
 for(i in 1:10^4){
-  z=rnorm(1, mean=-4, sd=sqrt(16)/50)
-  v[i] <- z
+  z=rnorm(50, mean=-4, sd=4)
+  z2 <- mean(z)
+  v[i] <- z2
   
 }
-# Presentamos en un histograma los valores obtenidos en el loop
-s = seq(0, 5, 0.1)
-hist(v,
-     main="Promedio estandarizados y distribución normal estándar",
-     breaks = 20,
-     xaxt="n",
-     ylab="Densidad",
-     xlab="x",
-     freq=FALSE)
+
+
+# Presentamos en un histograma los valores obtenidos
+h <- hist(v, breaks = 100, density = 10,
+          ylab = "Densidad", xlab = "x", main = "Promedio estandarizados y distribución normal estándar") 
+xfit <- seq(min(v), max(v), length = 40) 
+yfit <- dnorm(xfit, mean = mean(v), sd =sd(v)) 
+yfit <- yfit * diff(h$mids[1:2]) * length(v) 
+lines(xfit, yfit, col = "orange", lwd = 2)
+# Por último agregamos una leyenda para visualizar claramente los resultados  
+# y cambiamos la escala del eje
+axis(1, at=s,las=1)
+legend("topleft", 
+       legend = c("Promedio estandarizado",
+                  "Distribución normal estándar"),
+       lty = 1, col = c("grey","orange"), lwd =1, box.lty =1)
+
+
 # Sobre el histograma graficamos la densidad de la distribución normal estándar
 lines(s, 
-      dnorm(s, mean = -4, sd = sqrt(16)/50),
+      dnorm(s, mean = -4, sd = 16),
       lty = 1, 
       lwd = 2)
 
 polygon(s, 
-        dnorm(s, mean = -4, sd = sqrt(16)/50), 
+        dnorm(s, mean = -4, sd = 16), 
         col = rgb(1, 0, 0, alpha = 0.5))
 # Por último agregamos una leyenda para visualizar claramente los resultados  
 # y cambiamos la escala del eje
@@ -31,7 +41,6 @@ legend("topleft",
        legend = c("Promedio estandarizado",
                   "Distribución normal estándar"),
        lty = 1, col = c("grey","red"), lwd =1, box.lty =1)
-#Falta arreglarr NORMAL ESTANDAR
 #Parte c)
 #10^4 intervalos de confianza con confianza 95%
 n <- 50    # El tamaño válido de la muestra
@@ -71,7 +80,7 @@ for(Xn in v){
 }
 
 #X = numero de exitos de la muestra
-prop.test(no_rc_prop,n=50,p=0.9, alternative='less', conf.level = 0.95, correct= FALSE)
+prop.test(no_rc_prop,n=50,p=0.9, alternative='two.sided', conf.level = 0.95, correct= FALSE)
 
 
 #C)
